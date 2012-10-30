6b197e3a91a8e9b9ba65145d2358c89431b741c2
Vaibhav Puri <vaibhav@taggito.com>
2012-10-30 13:16:31 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitHubTutorial::Application.config.secret_token = 'fdbd8e910281b92a0ee1384a18ec3263a7c06398d649602499a2061684872346818eba6ade7a4dc97b083b90069675e45137e5c780cf039067be16859fdb4e4a'
